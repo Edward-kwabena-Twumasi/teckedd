6d06e94fc756d24a672d436893ea2e7ceb861cef
Edward <createdliving1000@gmail.com>
2022-08-15 17:01:17 +0000

    election dashboard with react and tailwind commit 1

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -4,12 +4,19 @@ const Page=()=>{
     return(
         
         <div className="page h-80  bg-[#cbd5e1] m-7 rounded-lg self-center shadow-md flex flex-col">
-        <div className="h-2/3 bg-slate-700 rounded-tl-lg rounded-tr-lg">
+            <div className="h-2/3  rounded-tl-lg rounded-tr-lg bg-no-repeat bg-center bg-cover" style={{ 
+                    backgroundImage: `url("https://picsum.photos/id/1/200/300")` 
+                    }}>
 
-        </div>
-        <h1 className="place-self-center h-1/3">
-            Hi i am a page
-        </h1>
+            </div>
+            <div className="place-self-start h-1/3 p-4">
+                <h1  className="place-self-start ">
+                    Machine learning
+                </h1>
+                <h2 className="place-self-start " >
+                I am a card representing a product about Machine learning
+                </h2>
+         </div>
         </div>
         
     );
